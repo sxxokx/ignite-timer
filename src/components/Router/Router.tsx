15aeefb735e24f44228a4b35915b01628d30ba67
Jean Borges <jean.angelo.borges@gmail.com>
2023-09-07 04:18:58 -0300

    v.4

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import { Home } from "../../pages/home"
-import { History } from "../../pages/History"
+import { History } from "../../pages/history"
 import { DefaultLayout } from "../layout"
 
 export const Router = () => {
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,10 +7,15 @@ export const Home = () => {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto"/>
+                    <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto"/>
+                    <datalist id="task-suggestions">
+                        <option value="projeto 1"/>
+                        <option value="projeto 2"/>
+                        <option value="projeto 3"/>
+                    </datalist>
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00"/>
+                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" step={5} min={5} max={60} />
 
                     <span>minutos.</span>
 
